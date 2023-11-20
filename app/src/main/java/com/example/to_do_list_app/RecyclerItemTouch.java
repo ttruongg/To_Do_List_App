51634d461e6a332106a9c34803f03ace5b4b3f01
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2023-11-20 15:05:04 +0700

    add recyclerview swipe behavior

---
+++ app/src/main/java/com/example/to_do_list_app/RecyclerItemTouch.java
@@ -0,0 +1,109 @@
+package com.example.to_do_list_app;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.to_do_list_app.Adapter.TaskAdapter;
+
+public class RecyclerItemTouch extends ItemTouchHelper.SimpleCallback {
+
+    private TaskAdapter adapter;
+
+
+    public RecyclerItemTouch(TaskAdapter adapter) {
+        super(0,ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+        this.adapter = adapter;
+    }
+
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+        int position = viewHolder.getAdapterPosition();
+        if (direction == ItemTouchHelper.LEFT){
+            AlertDialog.Builder dialog = new AlertDialog.Builder(adapter.getContext());
+            dialog.setTitle("Delete Task");
+            dialog.setMessage("Do you want to delete this task?");
+            dialog.setPositiveButton("Confirm",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            adapter.deleteTask(position);
+                        }
+                    });
+            dialog.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    adapter.notifyItemChanged(viewHolder.getAdapterPosition());
+                }
+            });
+            AlertDialog alertDialog = dialog.create();
+            alertDialog.show();
+        } else {
+            adapter.updateItem(position);
+        }
+
+    }
+
+    @Override
+    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+        Drawable icon;
+        ColorDrawable background;
+        View itemView = viewHolder.itemView;
+        int backgroundCornerOffset = 20;
+
+        if (dX>0){
+            icon = ContextCompat.getDrawable(adapter.getContext(), R.drawable.ic_edit);
+            background = new ColorDrawable(ContextCompat.getColor(adapter.getContext(), R.color.green));
+
+        } else{
+            icon = ContextCompat.getDrawable(adapter.getContext(), R.drawable.ic_delete);
+            background = new ColorDrawable(Color.RED);
+
+        }
+        assert icon != null;
+        int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconBottom = iconTop + icon.getIntrinsicHeight();
+
+        if (dX > 0) { // Swiping to the right
+            int iconLeft = itemView.getLeft() + iconMargin;
+            int iconRight = itemView.getLeft() + iconMargin + icon.getIntrinsicWidth();
+            icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+            background.setBounds(itemView.getLeft(), itemView.getTop(),
+                    itemView.getLeft() + ((int) dX) + backgroundCornerOffset, itemView.getBottom());
+        } else if (dX < 0) { // Swiping to the left
+            int iconLeft = itemView.getRight() - iconMargin - icon.getIntrinsicWidth();
+            int iconRight = itemView.getRight() - iconMargin;
+            icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+            background.setBounds(itemView.getRight() + ((int) dX) - backgroundCornerOffset,
+                    itemView.getTop(), itemView.getRight(), itemView.getBottom());
+        } else { // view is unSwiped
+            background.setBounds(0, 0, 0, 0);
+        }
+
+        background.draw(c);
+        icon.draw(c);
+
+
+
+    }
+}
--- app/src/main/java/com/example/to_do_list_app/Utils/DataBaseHandler.java
+++ app/src/main/java/com/example/to_do_list_app/Utils/DataBaseHandler.java
@@ -18,12 +18,12 @@ public class DataBaseHandler extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "ToDoListDataBase";
-    private static final String TABLE_TASK = "TaskToDo";
+    private static final String TABLE_TASK = "ToDoList";
     private static final String COLUMN_NAME_ID = "id";
     private static final String COLUMN_NAME_TASK = "task";
     private static final String COLUMN_NAME_STATUS = "status";
-    private static final String CREATE_TASK_TABLE = "CREATE TABLE" + TABLE_TASK + "(" + COLUMN_NAME_ID + "INTEGER PRIMARY KEY, "
-            + COLUMN_NAME_TASK + "TEXT, " + COLUMN_NAME_STATUS + " INTEGER)";
+    private static final String CREATE_TASK_TABLE = "CREATE TABLE " + TABLE_TASK + "(" + COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_NAME_TASK + " TEXT, "
+            + COLUMN_NAME_STATUS + " INTEGER)";
 
     private SQLiteDatabase db;
 
@@ -32,12 +32,13 @@ public class DataBaseHandler extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TASK_TABLE);
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
 
         onCreate(db);
@@ -58,28 +59,28 @@ public class DataBaseHandler extends SQLiteOpenHelper {
 
     public List<Task> getAllTask(){
         List<Task> taskList = new ArrayList<>();
-        Cursor cursor = null;
+        Cursor cur = null;
         db.beginTransaction();
         try{
-            cursor = db.query(TABLE_TASK, null, null, null, null, null, null, null);
-            if (cursor != null){
-                if (cursor.moveToFirst()){
+            cur = db.query(TABLE_TASK, null, null, null, null, null, null, null);
+            if(cur != null){
+                if(cur.moveToFirst()){
                     do{
                         Task task = new Task();
-                        task.setId(cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_NAME_ID)));
-                        task.setTask(cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NAME_TASK)));
-                        task.setTask(cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NAME_STATUS)));
+                        task.setId(cur.getInt(cur.getColumnIndexOrThrow(COLUMN_NAME_ID)));
+                        task.setTask(cur.getString(cur.getColumnIndexOrThrow(COLUMN_NAME_TASK)));
+                        task.setStatus(cur.getInt(cur.getColumnIndexOrThrow(COLUMN_NAME_STATUS)));
                         taskList.add(task);
-
-                    } while (cursor.moveToNext());
+                    }
+                    while(cur.moveToNext());
                 }
             }
-
-        } finally {
-            db.endTransaction();
-            cursor.close();
         }
-
+        finally {
+            db.endTransaction();
+            assert cur != null;
+            cur.close();
+        }
         return taskList;
     }
 
